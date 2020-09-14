9f4e30eb9a34d38ad965af2a8a6880d728707463
Masekolg <masekolg@gmail.com>
2020-09-14 13:38:00 +0200

    fixed syntax

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,13 @@
 const palindrome =(str)=>{
-    // let regex = /[^A-Za-z]/gi;
-    let regex = /\W\s/ig;
-    let newStr = str.replace(regex, '')
+    let regex = /[^A-Za-z]/gi;
+    
+    let newStr = str.toLowerCase().replace(regex, '')
     let strlength = newStr.length;
     for(let i = 0; i< strlength; i++){
-        if(str[i] !== str[strlength-1-i]){
+        if(str[i]!== str[strlength-1-i]){
             return "Nope, try another";
         }
     }
     return "Yay Palindrome";
 }
-console.log(palindrome('RaCeCar'))
+console.log(palindrome('racecar'))
